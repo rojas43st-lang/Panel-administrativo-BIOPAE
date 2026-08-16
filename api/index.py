a57a5bf591c68a6807694c150056a7f824048379
rojas43st-lang <rojas43st@gmail.com>
2026-08-16 22:44:52 +0000

    request

---
+++ api/index.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+app = FastAPI()
+
+# Archivos estáticos
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static"
+)
+
+# Plantillas HTML
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates"
+)
+
+
+@app.get("/admin", response_class=HTMLResponse)
+async def panel_admin(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
